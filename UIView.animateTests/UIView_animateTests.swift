9f5960e32d39965331fc919fa4a45de4eb949fff
Pasquale Bruno <pasqualebruno@mbpdipasquale2-001>
2017-12-09 18:24:14 +0100

    Initial Commit

---
+++ UIView.animateTests/UIView_animateTests.swift
@@ -0,0 +1,36 @@
+//
+//  UIView_animateTests.swift
+//  UIView.animateTests
+//
+//  Created by Pasquale Bruno on 09/12/2017.
+//  Copyright © 2017 Pasquale Bruno. All rights reserved.
+//
+
+import XCTest
+@testable import UIView_animate
+
+class UIView_animateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
